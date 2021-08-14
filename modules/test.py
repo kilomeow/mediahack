4c1a35e55e3c3aa655b5b675971313c4a70eab01
Kilo Meow <dest@disr.it>
2021-08-14 19:05:38 +0300

    test fix

--- modules/test.py
+++ modules/test.py
@@ -6,12 +6,12 @@ from modules.init import npc, score, modules_info
 content = StoryMap(
     entry=[
         score.instantiate(),
-        npc.Squirrel.info("""Это режим тестирования! Дополнительные команды:
-/addscore X - добавить X очков
-/vars - вывести значения всех переменных"""),
         npc.Squirrel.say("Пусть каждый тестировщик пришлет стикер. По итогу отправьте *Готово*"),
         npc.Squirrel.acquaintance(
             reply_phrases=["ОК"]),
+        npc.Squirrel.say("""Это режим тестирования! Дополнительные команды:
+/addscore X - добавить X очков
+/vars - вывести значения всех переменных"""),
         npc.Squirrel.ask("Модуль:", modules_info, Var.module)
     ]
 )
