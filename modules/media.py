26238b23a7ffb318b7a4479e6db4aebf9952dba8
Chimera <chimera@chimera.website>
2021-08-18 14:45:30 +0300

    Refactoring code, fix of a bug in "main" branch

--- modules/media.py
+++ modules/media.py
@@ -1,7 +1,7 @@
 from engine.glide import StoryMap
 from engine.var import Var, Proceed, Jump
 
-from modules.init import npc, score, docs, modules_info
+from modules.init import npc, score, docs
 
 description = "Социальные сети и мессенджеры"
 
@@ -223,10 +223,13 @@ content = StoryMap(
     ],
 
     game_break=[
-        npc.Squirrel.ask(
-            "Так, сейчас мы больше ничего не можем сделать. Постарайтесь чуть-чуть отдохнуть, заварите себе чай."
-            "Приступайте только когда почувствуете что есть силы продолжать игру.",
-            modules_info, Var.module),
+        npc.Squirrel.say(
+             "Так, сейчас мы больше ничего не можем сделать. Постарайтесь чуть-чуть отдохнуть, заварите себе чай."
+             "Приступайте только когда почувствуете что есть силы продолжать игру."),
+        # npc.Squirrel.ask(
+        #     "Так, сейчас мы больше ничего не можем сделать. Постарайтесь чуть-чуть отдохнуть, заварите себе чай."
+        #     "Приступайте только когда почувствуете что есть силы продолжать игру.",
+        #     modules_info, Var.module),
         # Proceed(vzlom=True)
         # Окей, я добавила меню в конце, но я не знаю, как именно оно работает
     ]
